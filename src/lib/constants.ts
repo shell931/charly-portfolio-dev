aad1143698156ab97f36aa35d3a002034d7fa4b7
developers-gateway-pro <165091308+developers-gateway-pro@users.noreply.github.com>
2024-11-20 10:45:24 -0500

    first commit

---
+++ src/lib/constants.ts
@@ -0,0 +1,13 @@
+export const LINKS = {
+  github: "https://github.com/shell931",
+  linkedin: "https://www.linkedin.com/in/carlosarriero",
+  medium: "",
+  discord: "",
+  dribble: "",
+};
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
